f7acfee421bc424a6dd8eac007fcce50f0acabf0
blavonne <luchik_sonza@mail.ru>
2020-11-22 13:59:54 +0300

    all 2 a v 2

--- ft_push_swap/srcs/all_to_a2.c
+++ ft_push_swap/srcs/all_to_a2.c
@@ -9,16 +9,19 @@ int			all_to_a2(t_stack **a, t_stack **b, t_info **info)
 	ft_bzero(&cur, sizeof(cur));
 	while (ptr)
 	{
-		to_top_rb(a, b, &cur);
-		to_top_rrb(a, b, &cur);
-		to_a_ra(a, b, &cur);
-		to_a_rra(a, b, &cur);
+		steps_top_rb(a, b, &cur);
+		steps_top_rrb(a, b, &cur);
+		steps_a_ra(a, b, &cur);
+		steps_a_rra(a, b, &cur);
 		set_rr(&cur);
 		if (!(*info)->place.way_to_a || (cur.steps_to_a + cur.steps_to_b <\
 		(*info)->place.steps_to_b + (*info)->place.steps_to_a))
 			(*info)->place = cur;
 		ptr = ptr->next;
 	}
+	do_rr(a, b, info);
+	to_top_b(a, b, info);
+	to_a(a, b, info);
 	if ((*b))
 		all_to_a2(a, b, info);
 	return (1);
