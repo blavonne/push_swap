dadad4eaa4071581b32c4bfdc65f8f2eef1ac080
blavonne <luchik_sonza@mail.ru>
2020-11-22 17:51:39 +0300

    new algo

--- ft_push_swap/srcs/all_to_b2.c
+++ ft_push_swap/srcs/all_to_b2.c
@@ -39,6 +39,55 @@ void			to_b_rb(t_stack **a, t_stack **b, t_info **info, int middle)
 	}
 }
 
+void			do_ra(t_stack **a, t_stack **b, t_info **info, int i)
+{
+	while (i--)
+	{
+		run_command("ra", a, 0);
+		if (!push_in_vector(&(*info)->cmd_c, RA, sizeof(char)))
+			clean_and_exit(a, b, info, 'm');
+	}
+}
+
+void			do_rra(t_stack **a, t_stack **b, t_info **info, int i)
+{
+	while (i--)
+	{
+		run_command("rra", a, 0);
+		if (!push_in_vector(&(*info)->cmd_c, RRA, sizeof(char)))
+			clean_and_exit(a, b, info, 'm');
+	}
+}
+
+void			do_rotate(t_stack **a, t_stack **b, t_info **info, int middle)
+{
+	int		len;
+	int		top;
+	int		bottom;
+	t_stack	*ptr;
+
+	ptr = (*a);
+	len = 0;
+	bottom = 0;
+	while (ptr)
+	{
+		if (!bottom && ptr->value < middle)
+		{
+			top = len;
+			bottom = -1;
+		}
+		if (bottom && ptr->value < middle)
+			bottom = len;
+		len++;
+		ptr = ptr->next;
+	}
+	bottom > 0 ? bottom = len - bottom : 0;
+	if (top <= bottom)
+		do_ra(a, b, info, top);
+	else
+		do_rra(a, b, info, bottom);
+}
+
 void			all_to_b(t_stack **a, t_stack **b, t_info **info)
 {
 	int				middle_val;
@@ -52,6 +101,8 @@ void			all_to_b(t_stack **a, t_stack **b, t_info **info)
 			try_sa(a, b, info);
 			to_b_rb(a, b, info, middle_val);
 			to_b_rrb(a, b, info, middle_val);
+			if (check_mid((*a), middle_val))
+				do_rotate(a, b, info, middle_val);
 		}
 	}
 	ft_printf("End cycle, ");
